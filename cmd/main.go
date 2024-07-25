9350a415aec13bfccce719e31c17da2dc672e30b
julianjackson <20231881+isjulianj@users.noreply.github.com>
2024-07-25 16:29:40 +0100

    create some flags and move set up to run func

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
 
+type Config struct {
+	Addr           int
+	Environment    string
+	AssetDirectory string
+}
+
+const (
+	defaultPort        = 6969
+	defaultEnvironment = "development"
+	defaultStaticDir   = "assets"
+)
+
 func main() {
-	err := http.ListenAndServe(":6969", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	run()
+}
+
+func run() {
+	var config Config
+
+	flag.IntVar(&config.Addr, "addr", defaultPort, "The server listen address")
+	flag.StringVar(&config.Environment, "env", defaultEnvironment, "The server environment")
+	flag.StringVar(&config.AssetDirectory, "assetDir", defaultStaticDir, "Static asset Directory")
+
+	flag.Parse()
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", config.Addr), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write([]byte("Hello World!")); err != nil {
 			panic(err)
 		}
@@ -15,4 +40,6 @@ func main() {
 	if err != nil {
 		fmt.Println("It's over anyways")
 	}
+
+	return
 }
